3de13620b8a0133b94dce2083994dbcdc2abf283
YoungKyuK <dudrb7795@gmail.com>
2025-03-19 18:42:27 +0900

    todoList

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,7 +1,55 @@
 import "./App.css";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+import { useState, useRef } from "react";
+
+// 데이터 모델링
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
-  return <>ToodoList</>;
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} />
+    </div>
+  );
 }
 
 export default App;
