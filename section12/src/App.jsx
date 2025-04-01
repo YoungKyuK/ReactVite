18429625f5f1e59b138f70898c963c72ebf8c75f
YoungKyuK <dudrb7795@gmail.com>
2025-04-01 14:20:49 +0900

    btncomponent

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,14 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
+import Button from "./components/Button";
+import Header from "./components/Header";
+
+import { getEmotionImage } from "./util/get-emotion-image";
+
+// 이미지가 많을 때 public에서 불러옴
+// 이미지가 적을 때 asstes에서 불러옴
+// asstes에 저장 시 : 메모리에 저장이 되있어서 다시 불러오지 않는다.
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -14,20 +22,36 @@ import Notfound from "./pages/Notfound";
 // 2. Routes컴포넌트 바깥 컴포넌트는 모든 페이지에 적용된다.
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
-
   return (
     <>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+      <Button
+        text={"123"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />
+
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />
+
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
